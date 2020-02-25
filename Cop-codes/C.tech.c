2ac72386d70cf98fab66ac708974fbdfc13b1585
Sumanth@//20 <54536286+sumanthks20@users.noreply.github.com>
2020-02-25 17:55:09 +0530

    Update C.tech.c

--- Cop-codes/C.tech.c
+++ Cop-codes/C.tech.c
@@ -3,21 +3,21 @@
 int main() {
 int n=5;
 int i ,j;
-for(i=0;i<=n;i++)
+for(i=0;i<n;i++)
 {
-    if(i==0||i==n)
+    if(i==0||i==n-1)
     {
-       for(j=0;j<n;j++) 
-       printf("*");
+       for(j=0;j<n;j++)
+       printf("* ");
     }
     else
     {
         for(j=0;j<=n;j++)
         {
             if(j==0||j==n)
-              printf("*");
+              printf("*\t");
             else
-              printf("\t");
+              printf("");
         }
     }
     printf("\n");
