1e2d5a3dd4bfeddd0f5b767e60fae00e6c642e09
Sumanth@//20 <54536286+sumanthks20@users.noreply.github.com>
2020-02-26 12:13:27 +0530

    Update C.tech.c

--- Cop-codes/C.tech.c
+++ Cop-codes/C.tech.c
@@ -10,7 +10,34 @@ for(i=0;i<n;i++)
        for(j=0;j<n;j++)
        printf("* ");
     }
-    else
+    else#include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter no of stars\n");
+    scanf("%d",&n);
+    int i ,j;
+    for(i=0;i<n;i++)
+    {
+        if(i==0||i==n-1)
+        {
+           for(j=0;j<n;j++)
+           printf("*");
+        }
+        else
+        {
+            for(j=0;j<=n;j++)
+            {
+                if(j==0||j==n)
+                  printf("*");
+                else
+                  printf("");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
     {
         for(j=0;j<=n;j++)
         {
